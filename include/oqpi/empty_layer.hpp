6c75b6e2f8a8cad756ebb88b63ee1c9c4a83c01b
Houssem Achouri <houssem.achouri@gmail.com>
2017-01-04 20:44:12 -0500

    Changed the way empty_layer is detected

--- include/oqpi/empty_layer.hpp
+++ include/oqpi/empty_layer.hpp
@@ -2,26 +2,16 @@
 
 namespace oqpi {
 
-    // Determines if two templates are the same
-    template<template<typename> typename T1, template<typename> typename T2>
-    struct is_same_template
-        : public std::false_type
-    {};
-    // Same!
-    template<template<typename> typename T>
-    struct is_same_template<T, T>
-        : public std::true_type
-    {};
-
-
     // Defines an empty layer for augmented interfaces
     template<typename>
     struct empty_layer {};
 
-    // Determines if a given layer is empty
+    // Determines if a given layer is empty: default case, not empty
     template<template<typename> typename _Layer>
-    struct is_empty_layer
-        : public is_same_template<_Layer, empty_layer>
-    {};
+    struct is_empty_layer : public std::false_type {};
+
+    // Determines if a given layer is empty: empty!
+    template<>
+    struct is_empty_layer<empty_layer> : public std::true_type {};
 
 } /*oqpi*/

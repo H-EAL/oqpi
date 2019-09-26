b3768829c4c13cf026b819b4109c550c916e0051
Houssem <houssem.achouri@gmail.com>
2019-09-26 10:10:53 -0400

    Put assert functions into oqpi namespace to avoid nameing conflicts

--- include/oqpi/error_handling.hpp
+++ include/oqpi/error_handling.hpp
@@ -16,12 +16,13 @@
 
 #if OQPI_ENABLE_CHECKS
 
-//----------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
 #define OQPI_ASSERT_DO_NOT_USE(Condition, File, Line)                                                   \
     {                                                                                                   \
         static bool always_ignore = false;                                                              \
         if (!always_ignore && !(Condition))                                                             \
         {                                                                                               \
+            using namespace oqpi;                                                                       \
             eAssertResult assertResult = assert_function(File, Line, #Condition);                       \
             if (assertResult == eAR_Retry)                                                              \
             {                                                                                           \
@@ -34,12 +35,13 @@
         }                                                                                               \
     }
 
-//----------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
 #define OQPI_ASSERTF_DO_NOT_USE(Condition, Msg, File, Line, ...)                                        \
     {                                                                                                   \
         static bool always_ignore = false;                                                              \
         if (!always_ignore && !(Condition))                                                             \
         {                                                                                               \
+            using namespace oqpi;                                                                       \
             eAssertResult assertResult = assert_function(File, Line, #Condition, Msg, ##__VA_ARGS__);   \
             if (assertResult == eAR_Retry)                                                              \
             {                                                                                           \
@@ -52,117 +54,121 @@
         }                                                                                               \
     }
 
-//----------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
 // Always evaluate the condition
 #define oqpi_verify(COND)               OQPI_ASSERT_DO_NOT_USE((COND), __FILE__, __LINE__)
 #define oqpi_verifyf(COND, MSG, ...)    OQPI_ASSERTF_DO_NOT_USE((COND), (MSG), __FILE__, __LINE__, ##__VA_ARGS__)
-//----------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
 // Assertion that will cause the program to crash (can be skipped)
 #define oqpi_check(COND)                OQPI_ASSERT_DO_NOT_USE((COND), __FILE__, __LINE__)
 #define oqpi_checkf(COND, MSG, ...)     OQPI_ASSERTF_DO_NOT_USE((COND), (MSG), __FILE__, __LINE__, ##__VA_ARGS__)
-//----------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
 // Assertion used inside an if statement that will output an error log and evaluates to false
 // if the condition is not met. Always returns true when deactivated.
-#define oqpi_ensure(COND)               ((COND) ? true : assert_and_return_false(__FILE__, __LINE__, #COND))
-#define oqpi_ensuref(COND, MSG, ...)    ((COND) ? true : assert_and_return_false(__FILE__, __LINE__, #COND, (MSG), ##__VA_ARGS__))
-//----------------------------------------------------------------------------------------------
+#define oqpi_ensure(COND)               ((COND) ? true : oqpi::assert_and_return_false(__FILE__, __LINE__, #COND))
+#define oqpi_ensuref(COND, MSG, ...)    ((COND) ? true : oqpi::assert_and_return_false(__FILE__, __LINE__, #COND, (MSG), ##__VA_ARGS__))
+//--------------------------------------------------------------------------------------------------
 // Assertion used inside an if statement that will output an error log and evaluates to true
 // if the condition is not met. Always returns false when deactivated.
-#define oqpi_failed(COND)               ((COND) ? false : assert_and_return_true(__FILE__, __LINE__, #COND))
-#define oqpi_failedf(COND, MSG, ...)    ((COND) ? false : assert_and_return_true(__FILE__, __LINE__, #COND, (MSG), ##__VA_ARGS__))
-//----------------------------------------------------------------------------------------------
-
-
-//----------------------------------------------------------------------------------------------
-enum eAssertResult
-{
-    eAR_Abort,
-    eAR_Retry,
-    eAR_Ignore,
-    eAR_AlwaysIgnore
-};
+#define oqpi_failed(COND)               ((COND) ? false : oqpi::assert_and_return_true(__FILE__, __LINE__, #COND))
+#define oqpi_failedf(COND, MSG, ...)    ((COND) ? false : oqpi::assert_and_return_true(__FILE__, __LINE__, #COND, (MSG), ##__VA_ARGS__))
 //--------------------------------------------------------------------------------------------------
-inline eAssertResult assert_function(const char *file, int line, const char *expression, const char *description = "", ...)
-{
-    std::string strMessage;
-
-    va_list argPtr;
-    static const unsigned int bufferSize = 1 << 10;
-    thread_local static char formattedMessage[bufferSize];
-    va_start(argPtr, description);
-    vsprintf_s(formattedMessage, bufferSize, description, argPtr);
-    va_end(argPtr);
 
 
-    strMessage = std::string(
-        "*************************************************************************\n"
-        "Assertion failed:\n"
-        "=================\n"
-        "[" + std::string(expression) + "]\n"
-        "-------------------------------------------------------------------------\n"
-        + formattedMessage + "\n"
-        "-------------------------------------------------------------------------\n"
-        + file + ", line " + std::to_string(line) + "\n\n"
-        "*************************************************************************\n"
-        "Stack trace:\n"
-        "-------------------------------------------------------------------------\n");
+namespace oqpi {
+
+    //----------------------------------------------------------------------------------------------
+    enum eAssertResult
+    {
+        eAR_Abort,
+        eAR_Retry,
+        eAR_Ignore,
+        eAR_AlwaysIgnore
+    };
+    //----------------------------------------------------------------------------------------------
+    inline eAssertResult assert_function(const char *file, int line, const char *expression, const char *description = "", ...)
+    {
+        std::string strMessage;
+
+        va_list argPtr;
+        static const unsigned int bufferSize = 1 << 10;
+        thread_local static char formattedMessage[bufferSize];
+        va_start(argPtr, description);
+        vsprintf_s(formattedMessage, bufferSize, description, argPtr);
+        va_end(argPtr);
+
+
+        strMessage = std::string(
+            "*************************************************************************\n"
+            "Assertion failed:\n"
+            "=================\n"
+            "[" + std::string(expression) + "]\n"
+            "-------------------------------------------------------------------------\n"
+            + formattedMessage + "\n"
+            "-------------------------------------------------------------------------\n"
+            + file + ", line " + std::to_string(line) + "\n\n"
+            "*************************************************************************\n"
+            "Stack trace:\n"
+            "-------------------------------------------------------------------------\n");
 
 #if OQPI_PLATFORM_WIN
-    eAssertResult assertResult = eAR_Ignore;
-    int result = MessageBoxA(0, strMessage.c_str(), "Assertion Failed", MB_ABORTRETRYIGNORE);
-    switch (result)
-    {
-    case IDABORT:
-        assertResult = eAR_Abort;
-        exit(1);
-        break;
+        eAssertResult assertResult = eAR_Ignore;
+        int result = MessageBoxA(0, strMessage.c_str(), "Assertion Failed", MB_ABORTRETRYIGNORE);
+        switch (result)
+        {
+        case IDABORT:
+            assertResult = eAR_Abort;
+            exit(1);
+            break;
 
-    case IDRETRY:
-        assertResult = eAR_Retry;
-        break;
+        case IDRETRY:
+            assertResult = eAR_Retry;
+            break;
 
-    case IDIGNORE:
-        assertResult = (GetAsyncKeyState(VK_SHIFT) & 0x8000) ? eAR_AlwaysIgnore : eAR_Ignore;
-        break;
-    }
+        case IDIGNORE:
+            assertResult = (GetAsyncKeyState(VK_SHIFT) & 0x8000) ? eAR_AlwaysIgnore : eAR_Ignore;
+            break;
+        }
 
-    return assertResult;
+        return assertResult;
 
 #else
-    std::cout << strMessage;
-    return eAR_Retry;
+        std::cout << strMessage;
+        return eAR_Retry;
 #endif // OQPI_PLATFORM_WIN
-}
-//--------------------------------------------------------------------------------------------------
-inline void debugger_break()
-{
+    }
+    //----------------------------------------------------------------------------------------------
+    inline void debugger_break()
+    {
 #if (OQPI_PLATFORM_WIN)
-    __debugbreak();
+        __debugbreak();
 #else
-    asm("int $3");
+        asm("int $3");
 #endif
-}
-//--------------------------------------------------------------------------------------------------
-inline bool assert_and_return_false(const char *file, int line, const char *expression, const char *description = "", ...)
-{
-    const eAssertResult assertResult = assert_function(file, line, expression, description);
-    if (assertResult == eAR_Retry)
-    {
-        debugger_break();
     }
-    return false;
-}
-//--------------------------------------------------------------------------------------------------
-inline bool assert_and_return_true(const char *file, int line, const char *expression, const char *description = "", ...)
-{
-    const eAssertResult assertResult = assert_function(file, line, expression, description);
-    if (assertResult == eAR_Retry)
+    //----------------------------------------------------------------------------------------------
+    inline bool assert_and_return_false(const char *file, int line, const char *expression, const char *description = "", ...)
     {
-        debugger_break();
+        const eAssertResult assertResult = assert_function(file, line, expression, description);
+        if (assertResult == eAR_Retry)
+        {
+            debugger_break();
+        }
+        return false;
     }
-    return true;
-}
-//--------------------------------------------------------------------------------------------------
+    //----------------------------------------------------------------------------------------------
+    inline bool assert_and_return_true(const char *file, int line, const char *expression, const char *description = "", ...)
+    {
+        const eAssertResult assertResult = assert_function(file, line, expression, description);
+        if (assertResult == eAR_Retry)
+        {
+            debugger_break();
+        }
+        return true;
+    }
+    //----------------------------------------------------------------------------------------------
+
+} /*oqpi*/
 
 #else
 

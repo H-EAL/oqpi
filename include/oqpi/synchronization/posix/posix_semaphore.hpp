bca9a3c6446b1ed039aef37f225a3d17b7256a41
nathalie <nathalie.raffray9@gmail.com>
2022-10-31 15:28:43 -0400

    Fixes for posix_event and posix_semaphore
    When they fail at creation, I make sure to release them. During release I unlink named semaphores so that they don't persist after program exits. However the first time I unlink a named semaphore, any other existing ones will not be connected to the name anymore. So if I try open a semaphore with that name, it will create a new one.

--- include/oqpi/synchronization/posix/posix_semaphore.hpp
+++ include/oqpi/synchronization/posix/posix_semaphore.hpp
@@ -28,45 +28,54 @@ namespace oqpi {
         posix_semaphore(const std::string &name, sync_object_creation_options creationOption, int32_t initCount, int32_t maxCount)
                 : maxCount_(maxCount)
                 , handle_(nullptr)
-                , isLocalSyncObject_(name.empty())
+                , name_(name)
         {
-            if(isLocalSyncObject_ && creationOption != sync_object_creation_options::open_existing)
+            const auto isLocalSyncObject = name_.empty();
+            if(isLocalSyncObject && creationOption != sync_object_creation_options::open_existing)
             {
                 // Create an unnamed semaphore.
                 handle_ = new sem_t;
 
-                const auto error = sem_init(handle_, 0, 0);
+                const auto error = sem_init(handle_, 0, initCount);
                 if(error == -1)
                 {
                     oqpi_error("sem_init failed with error %d", errno);
+                    release();
                 }
             }
             else
             {
                 // Create a named semaphore.
-                if (oqpi_failed(isNameValid(name)))
+                if (oqpi_failed(isNameValid()))
                 {
                     oqpi_error("the name \"%s\" you provided is not valid for a posix semaphore.", name.c_str());
                     return;
                 }
 
-                // Create a new semaphore.
-                if (creationOption == sync_object_creation_options::create_if_nonexistent)
-                {
-                    // An error is returned if a semaphore with the given name already exists.
-                    handle_ = sem_open(name.c_str(), O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, initCount);
+                // If both O_CREAT and O_EXCL are specified, then an error is returned if a semaphore with the given
+                // name already exists. Otherwise it creates it.
+                handle_ = sem_open(name.c_str(), O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, initCount);
 
-                }
-                // Create or open a semaphore.
-                else
+                if(handle_ != SEM_FAILED && creationOption == sync_object_creation_options::open_existing)
                 {
-                    // If a semaphore with name doesn't exist, then it is created.
-                    handle_ = sem_open(name.c_str(), O_CREAT, S_IRUSR | S_IWUSR, initCount);
+                    // We've created a semaphore even though given the open_existing creation option.
+                    oqpi_error("Trying to open an existing semaphore that doesn't exist.");
+                    release();
+                    return;
+                }
+
+                // Open a semaphore
+                if(handle_ == SEM_FAILED &&
+                   (creationOption == sync_object_creation_options::open_existing
+                    || creationOption == sync_object_creation_options::open_or_create))
+                {
+                    handle_ = sem_open(name.c_str(), O_CREAT, S_IRUSR | S_IWUSR, 0);
                 }
 
                 if (handle_ == SEM_FAILED)
                 {
                     oqpi_error("sem_open failed with error %d", errno);
+                    release();
                 }
             }
         }
@@ -74,25 +83,13 @@ namespace oqpi {
         //------------------------------------------------------------------------------------------
         ~posix_semaphore()
         {
-            if (handle_)
-            {
-                if(isLocalSyncObject_)
-                {
-                    sem_destroy(handle_);
-                    delete handle_;
-                }
-                else
-                {
-                    sem_close(handle_);
-                }
-                handle_ = nullptr;
-            }
+            release();
         }
 
         //------------------------------------------------------------------------------------------
         posix_semaphore(posix_semaphore &&other)
                 : handle_(other.handle_)
-                , isLocalSyncObject_(other.isLocalSyncObject_)
+                , name_(other.name_)
                 , maxCount_(other.maxCount_)
         {
             other.handle_ = nullptr;
@@ -104,7 +101,7 @@ namespace oqpi {
             if (this != &rhs && !isValid())
             {
                 handle_             = rhs.handle_;
-                isLocalSyncObject_  = rhs.isLocalSyncObject_;
+                name_               = rhs.name_;
                 maxCount_           = rhs.maxCount_;
                 rhs.handle_         = nullptr;
             }
@@ -189,12 +186,32 @@ namespace oqpi {
 
     private:
         //------------------------------------------------------------------------------------------
-        bool isNameValid(const std::string &name) const
+        void release()
+        {
+            if (handle_)
+            {
+                const auto isLocalSyncObject = name_.empty();
+                if(isLocalSyncObject)
+                {
+                    sem_destroy(handle_);
+                    delete handle_;
+                }
+                else
+                {
+                    sem_close(handle_);
+                    sem_unlink(name_.c_str());
+                }
+                handle_ = nullptr;
+            }
+        }
+
+        //------------------------------------------------------------------------------------------
+        bool isNameValid() const
         {
             // Note that name must be in the form of /somename; that is, a null-terminated string of up to NAME_MAX
             // characters consisting of an initial slash, followed by one or more characters, none of which are slashes.
-            return (name.length() < NAME_MAX && name.length() > 1 && name[0] == '/'
-                    && std::find(name.begin() + 1, name.end(), '/') == name.end());
+            return (name_.length() < NAME_MAX && name_.length() > 1 && name_[0] == '/'
+                    && std::find(name_.begin() + 1, name_.end(), '/') == name_.end());
         }
 
     private:
@@ -205,9 +222,9 @@ namespace oqpi {
 
     private:
         //------------------------------------------------------------------------------------------
-        int     maxCount_;
-        sem_t*  handle_;
-        bool    isLocalSyncObject_;
+        int         maxCount_;
+        sem_t*      handle_;
+        std::string name_;
     };
 
 } /*oqpi*/

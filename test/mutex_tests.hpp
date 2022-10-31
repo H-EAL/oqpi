7fb69e292c2115acb1a4eed0fe6f17f9b2940976
nathalie <nathalie.raffray9@gmail.com>
2022-10-31 15:32:52 -0400

    Update the names of mutexes to make them valid in posix

--- test/mutex_tests.hpp
+++ test/mutex_tests.hpp
@@ -3,13 +3,13 @@ TEST_CASE("Mutexes.", "[mutex]")
 {
     SECTION("Global Mutex.")
     {
-        auto mutex = oqpi::global_mutex("Global\\oqpiTestMutex", oqpi::sync_object_creation_options::open_existing);
+        auto mutex = oqpi::global_mutex("/Global\\oqpiTestMutex", oqpi::sync_object_creation_options::open_existing);
         REQUIRE(!mutex.isValid());
 
-        mutex = oqpi::global_mutex("Global\\oqpiTestMutex", oqpi::sync_object_creation_options::create_if_nonexistent);
+        mutex = oqpi::global_mutex("/Global\\oqpiTestMutex", oqpi::sync_object_creation_options::create_if_nonexistent);
         REQUIRE(mutex.isValid());
 
-        auto mutex2 = oqpi::global_mutex("Global\\oqpiTestMutex", oqpi::sync_object_creation_options::open_existing);
+        auto mutex2 = oqpi::global_mutex("/Global\\oqpiTestMutex", oqpi::sync_object_creation_options::open_existing);
         REQUIRE(mutex2.isValid());
 
         mutex.unlock();

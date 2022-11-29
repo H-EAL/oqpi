e01c30db7aff05e40d8627ea3a9a3db981deafd8
nathalie_raffray <nathalie@3dverse.com>
2022-11-29 14:27:48 -0500

    Updated threading_tests to not test coreAffinity with cores that don't exist on user's machine.

--- test/threading_tests.hpp
+++ test/threading_tests.hpp
@@ -1,10 +1,20 @@
 
 //--------------------------------------------------------------------------------------------------
+static oqpi::core_affinity coreNumberToAffinity(uint32_t coreNumber)
+{
+    return static_cast<oqpi::core_affinity>(pow(2, coreNumber));
+}
+
+//--------------------------------------------------------------------------------------------------
 TEST_CASE("Threading.", "[threading]")
 {
     // oqpi::thread() will create a non-joinable thread.
     // If we try to detach or join it, it should simply issue a warning. 
     CHECK(oqpi::thread().joinable() == false);
+
+    const auto numLogicalCores = oqpi::thread::hardware_concurrency();
+    // I assume that there are least two cores to test with below.
+    REQUIRE(numLogicalCores >= 2);
 
     auto coreAffinityMask       = oqpi::core_affinity(oqpi::core0 | oqpi::core1);
     // The maximal stack size of the thread, 0 will use the system's default value
@@ -20,12 +30,17 @@ TEST_CASE("Threading.", "[threading]")
      CHECK(t.getCoreAffinityMask() == coreAffinityMask);
      CHECK(t.getPriority() == priority);
 
-     coreAffinityMask = oqpi::core_affinity(oqpi::core3);
+     auto coreNum = numLogicalCores - 1;
+     coreAffinityMask = coreNumberToAffinity(coreNum);
+
      t.setCoreAffinityMask(coreAffinityMask);
      CHECK(t.getCoreAffinityMask() == coreAffinityMask);
 
-     t.setCoreAffinity(4);
-     CHECK(t.getCoreAffinityMask() == oqpi::core_affinity(oqpi::core4));
+     coreNum = numLogicalCores - 2;
+     coreAffinityMask = coreNumberToAffinity(coreNum);
+
+    t.setCoreAffinity(coreNum);
+     CHECK(t.getCoreAffinityMask() == coreAffinityMask);
 
      priority = oqpi::thread_priority::above_normal;
      t.setPriority(priority);
